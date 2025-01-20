24d202e124b0d748b8a49954beab0aba1eaa8c52
abelureste <aureste03@gmail.com>
2025-01-20 02:59:47 -0500

    Added JavaDemo file

---
+++ 3200 Data Structures/JavaDemo/src/CSCI3200/JavaDemo.java
@@ -0,0 +1,21 @@
+package CSCI3200;
+
+import java.util.Scanner;
+
+public class JavaDemo {
+    //public static int basePowerCalculation(int m, int n) {
+        //add a base case
+
+
+        //add a recursive statement
+
+    //}
+
+    public static void main(String[] args) throws Exception {
+        Scanner base = new Scanner(System.in);
+        System.out.println("Enter base number:");
+
+        Scanner power = new Scanner(System.in);
+        System.out.println("Enter power number:");
+    }
+}
