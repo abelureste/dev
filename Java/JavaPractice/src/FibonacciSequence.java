c5c19b456fe129adefc072a0d781f011955ca997
abelureste <aureste03@gmail.com>
2025-01-25 16:05:04 -0500

    Created FibonacciSequence

---
+++ Java/JavaPractice/src/FibonacciSequence.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class FibonacciSequence {
+    public static void main(String[] args) {
+        Scanner userInput = new Scanner(System.in);
+        System.out.println("What n");
+    }
+
+    public static int FibSequence(int number) {
+        int answer = 0;
+        return answer;
+    }
+}
