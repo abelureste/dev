b79400d6e60367279e70f6f4a4e949706769a46f
abelureste <aureste03@gmail.com>
2025-01-26 22:11:51 -0500

    Updated FibonacciSequence

--- Java/JavaPractice/src/FibonacciSequence.java
+++ Java/JavaPractice/src/FibonacciSequence.java
@@ -3,10 +3,11 @@ import java.util.Scanner;
 public class FibonacciSequence {
     public static void main(String[] args) {
         Scanner userInput = new Scanner(System.in);
-        System.out.println("What n");
+        System.out.println("what n");
+        
     }
 
-    public static int FibSequence(int number) {
+    public static int FibSequence(int number) {     //Generate Fibonnaci's sequence for as many times as the number is entered.
         int answer = 0;
         return answer;
     }
