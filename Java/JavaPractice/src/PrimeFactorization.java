ffa819cdd3418a9dddf94eb657dcdf58e8ce27ff
abelureste <aureste03@gmail.com>
2025-01-31 15:31:48 -0500

    Updated PrimeFactorization

--- Java/JavaPractice/src/PrimeFactorization.java
+++ Java/JavaPractice/src/PrimeFactorization.java
@@ -7,15 +7,31 @@ public class PrimeFactorization {
         Scanner userInput = new Scanner(System.in);
         System.out.print("Enter a number: ");
         int number = userInput.nextInt();
+        userInput.close();
 
+        if (number > 1) {
+            System.out.print("Prime factors: ");
+            primeFactorization(number);
+        } else {
+            System.out.println("Enter a number greater than 1.");
+        }
     }
 
-    public static int primeFactorization(int number) {
-        while(number % 2 == 0) {
-            
+    public static void primeFactorization(int num) {
+        while(num % 2 == 0) {
+            System.out.print(2 + " ");
+            num /= 2;
         }
 
-        for(int i = 0; i > number; i++) {
-            int primeNumber = number / i;
+        for(int i = 3; i * i <= num; i += 2) {
+            while (num % i == 0) {
+                System.out.print(i + " ");
+                num /= i;
+            }
+        }
+
+        if (num > 1) {
+            System.out.print(num);
+        }
     }
 }
