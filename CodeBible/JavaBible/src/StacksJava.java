a5feb6530b43867fdca30fa85dd0fabd87eb3527
abelureste <aureste03@gmail.com>
2025-02-03 16:49:46 -0500

    Created StacksJava

---
+++ CodeBible/JavaBible/src/StacksJava.java
@@ -0,0 +1,26 @@
+import java.util.Stack;
+
+public class StacksJava {
+    public static void main(String[] args) {
+        Stack<String> stack = new Stack<String>();          //Initialize stack
+
+        stack.push("Minecraft");                       //Push elements into stack
+        stack.push("Skyrim");                       
+        stack.push("DOOM");
+        stack.push("Borderlands");
+        stack.push("Red Dead Redemption");
+
+        System.out.println(stack);                          //Returns stack
+
+        stack.pop();                                        //Removes top element from stack
+        stack.pop();
+
+        System.out.println(stack.peek());                   //Returns top element in stack
+
+        System.out.println(stack.isEmpty());                //Returns true if stack is empty, else returns false
+
+        System.out.println(stack.search("DOOM"));         //Returns the index of the element in the stack else returns -1
+        System.out.println(stack.search("Fallout"));
+    }    
+
+}
