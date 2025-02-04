c15c94897f12a213f1b3b601292b677b0bc1044f
Abel Ureste <aureste03@gmail.com>
2025-02-04 12:13:40 -0500

    Created SoftwareEngineering

---
+++ CodeBible/PythonBible/SoftwareEngineering.py
@@ -0,0 +1,18 @@
+
+print("( Gradebook Program | Software Engineering )")
+
+class student:
+    def __init__(self, studentName: str, studentGrade: float):
+        self.studentName = studentName
+        self.studentGrade = studentGrade
+
+    def averageGrade(self):
+        return
+
+
+studentNames = []
+
+for x in range(len(studentNames)):
+    studentNames.append = input("Enter the student names: ")
+
+print(studentNames)
