730951a810e9674e5731df76034df43ee09e643d
abelureste <aureste03@gmail.com>
2025-02-04 18:08:29 -0500

    Created Queues

---
+++ CodeBible/JavaBible/src/Queues.java
@@ -0,0 +1,18 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class Queues {
+    public static void main (String[] args) {
+        Queue<String> queue = new LinkedList<String>();
+        
+        queue.offer("Karen");
+        queue.offer("Chad");
+        queue.offer("Steve");
+        queue.offer("Harold");
+
+        queue.poll();
+        queue.poll();
+
+        System.out.println(queue.peek());
+    }
+}
--- CodeBible/JavaBible/src/Stacks.java
+++ CodeBible/JavaBible/src/Stacks.java
@@ -1,6 +1,6 @@
 import java.util.Stack;
 
-public class StacksJava {
+public class Stacks {
     public static void main(String[] args) {
         Stack<String> stack = new Stack<String>();          //Initialize stack
 
