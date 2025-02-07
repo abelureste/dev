965bb2caddd455b7f00de57ba81503d8ca939d4d
abelureste <aureste03@gmail.com>
2025-02-07 18:19:41 -0500

    Updated Chapter2Homework

--- 3200 Data Structures/JavaDemo/src/CSCI3200/Chapter2Homework.java
+++ 3200 Data Structures/JavaDemo/src/CSCI3200/Chapter2Homework.java
@@ -2,23 +2,26 @@ package CSCI3200;
 
 public class Chapter2Homework {
     public static int alg1(int n) {
-        int steps = 0;
-        System.out.println("N is: " + n);
-        for (int k = 1; k < n; k *= 2) {
-            System.out.println("Step " + steps + ", K = " + k);
-            steps++;
+        int steps = 0;                          //O(1)
+        System.out.println("N is: " + n);       //O(1)
+        for (int k = 1; k < n; k *= 2) {        //O(log n)
+            System.out.println("Step " + steps + ", K = " + k);     //O(1)
+            steps++;    //O(1)
         }
-        return steps;
+        return steps;   //O(1)
     }
 
     public static int alg2(int n) {
-        int steps = 0;
-        for (int i = 0; i < n * n; i++){
-            for (int j = 0; j < n; j*= 2){
-            steps++;
+        int steps = 0;      //O(1)
+        System.out.println("N is: " + n);    //O(1)
+        for (int i = 0; i < n * n; i++){     //O(n^2)
+            System.out.println("[Outer loop] Iteration " + i);     //O(1)
+            for (int j = 1; j < n; j*= 2){   //O(log n)
+                System.out.println("[Inner loop] Step " + steps + ", J = " + j);  //O(1)
+            steps++;    //O(1)
             }
          }
-        return steps;
+        return steps;   //O(1)
     }
         
     public static int alg3(int n){
@@ -33,7 +36,8 @@ public class Chapter2Homework {
     }
 
     public static void main (String[] args) {
-        //System.out.print(alg1(100));          //Only returns the final step #
+        
         alg1(100);
+        alg2(10);
     }
 }
