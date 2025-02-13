fe508990561a175a9befa56cbd9df7de536c0476
Abel Ureste <aureste03@gmail.com>
2025-02-13 10:22:31 -0500

    Created Spring3200Lab1

---
+++ 3200 Data Structures/JavaDemo/src/CSCI3200Lab1Assignment/PrintNode.java
@@ -0,0 +1,31 @@
+package CSCI3200Lab1Assignment;
+
+public class PrintNode{
+    private int job;
+    private PrintNode link;
+    public PrintNode () {
+        job = 0;
+        link = null;
+    }
+    public PrintNode (int job) {
+        this.job = job;
+        link = null;
+    }
+    public PrintNode(int job, PrintNode link) {
+        this.job = job;
+        this.link = link;
+    }
+    public void setLink (PrintNode newLink) {
+        link = newLink;
+    }
+    public void setJob (int newJob) {
+        job = newJob;
+    }
+    public PrintNode getLink () {
+        return link;
+    }
+    public int getJob () {
+        return job;
+    }
+}
+
---
+++ 3200 Data Structures/JavaDemo/src/CSCI3200Lab1Assignment/Spring3200Lab1.java
@@ -0,0 +1,22 @@
+package CSCI3200Lab1Assignment;
+
+import java.util.LinkedList;
+
+public class Spring3200Lab1 {
+    public static void main (String[] args) {
+        LinkedList<String> myLinkedList = new LinkedList<String>();
+        myLinkedList.add("3200");
+        myLinkedList.addFirst("CSCI");
+        myLinkedList.add(":");
+        myLinkedList.add(3, "D1");
+        System.out.println(myLinkedList);
+        myLinkedList.add("   ");
+        System.out.println(myLinkedList + " List Size:" + myLinkedList.size());
+        myLinkedList.add("Spring 2025");
+        myLinkedList.add("Algorithm Analysis");
+        myLinkedList.add(6, "Data Structure");
+        System.out.println(myLinkedList.get(6));
+        System.out.println(myLinkedList.getLast());
+        System.out.println(myLinkedList + " List size: " + myLinkedList.size());
+    }
+}
