1382d046cf7f83b3c6a4df858ef406268572579b
abelureste <aureste03@gmail.com>
2025-02-17 16:28:09 -0500

    Completed Spring3200Lab1

--- 3200 Data Structures/JavaDemo/src/CSCI3200Lab1Assignment/PrintNode.java
+++ 3200 Data Structures/JavaDemo/src/CSCI3200Lab1Assignment/PrintNode.java
@@ -2,7 +2,7 @@ package CSCI3200Lab1Assignment;
 
 public class PrintNode<Type>{
     private String job;
-    private Type link;
+    private PrintNode<Type> link;
     public PrintNode () {
         job = "";
         link = null;
@@ -11,17 +11,17 @@ public class PrintNode<Type>{
         this.job = job;
         link = null;
     }
-    public PrintNode(String job, Type link) {
+    public PrintNode(String job, PrintNode<Type> link) {
         this.job = job;
         this.link = link;
     }
-    public void setLink (Type newLink) {
+    public void setLink (PrintNode<Type> newLink) {
         link = newLink;
     }
     public void setJob (String newJob) {
         job = newJob;
     }
-    public Type getLink () {
+    public PrintNode<Type> getLink () {
         return link;
     }
     public String getJob () {
--- 3200 Data Structures/JavaDemo/src/CSCI3200Lab1Assignment/Spring3200Lab1.java
+++ 3200 Data Structures/JavaDemo/src/CSCI3200Lab1Assignment/Spring3200Lab1.java
@@ -3,8 +3,21 @@ package CSCI3200Lab1Assignment;
 import java.util.LinkedList;
 
 public class Spring3200Lab1 {
+    public static <Type> void PrintAllNodes(PrintNode<String> job) {        //Method that prints all nodes connected to the job inputted
+        PrintNode<String> current = job;
+        while (current != null) {
+            System.out.print(current.getJob());
+            if (current.getLink() != null) {
+                System.out.print(" --> ");
+            }
+            current = current.getLink();
+        }
+        System.out.println("");
+    }
+
     public static void main (String[] args) {
         //Question 1
+        System.out.println("-------- Question 1 --------");
         LinkedList<String> myLinkedList = new LinkedList<String>();
         myLinkedList.add("3200");
         myLinkedList.addFirst("CSCI");
@@ -12,7 +25,7 @@ public class Spring3200Lab1 {
         myLinkedList.add(3, "D1");
         System.out.println(myLinkedList);
         myLinkedList.add("   ");
-        System.out.println(myLinkedList + " List Size:" + myLinkedList.size());
+        System.out.println(myLinkedList + " List Size: " + myLinkedList.size());
         myLinkedList.add("Spring 2025");
         myLinkedList.add("Algorithm Analysis");
         myLinkedList.add(6, "Data Structure");
@@ -21,6 +34,8 @@ public class Spring3200Lab1 {
         System.out.println(myLinkedList + " List size: " + myLinkedList.size());
 
         //Question 2
+        System.out.println("");
+        System.out.println("-------- Question 2 --------");
         PrintNode<String> job1 = new PrintNode<String>("College");
         PrintNode<String> job2 = new PrintNode<String>("Department");
         PrintNode<String> job3 = new PrintNode<String>("Professor");
@@ -31,5 +46,18 @@ public class Spring3200Lab1 {
         PrintNode<String> job8 = new PrintNode<String>("D2L");
         PrintNode<String> job9 = new PrintNode<String>("Provost");
         
+        job1.setLink(job2);
+        job2.setLink(job6);
+        job3.setLink(job6);
+        job4.setLink(job7);
+        job5.setLink(job6);
+        job6.setLink(job7);
+        job7.setLink(job8);
+        job9.setLink(job5);
+
+        PrintAllNodes(job2);
+        PrintAllNodes(job1);
+        PrintAllNodes(job4);
+        PrintAllNodes(job6);
     }
 }
