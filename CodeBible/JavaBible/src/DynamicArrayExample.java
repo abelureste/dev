cec5bde98995400b3906bc47e2d6e5c3d1384f2b
abelureste <aureste03@gmail.com>
2025-02-17 17:39:48 -0500

    Updated DynamicArrayExample

--- CodeBible/JavaBible/src/DynamicArrayExample.java
+++ CodeBible/JavaBible/src/DynamicArrayExample.java
@@ -1,5 +1,24 @@
+import java.util.ArrayList;
+
 public class DynamicArrayExample {
     public static void main (String[] args) {
-        
+        ArrayList<Integer> dynamicArray = new ArrayList<Integer>();
+        dynamicArray.add(12);
+        dynamicArray.add(24);
+        dynamicArray.add(36);
+        dynamicArray.add(48);
+
+        System.out.println(dynamicArray.size());
+        System.out.println(dynamicArray.get(2));
+
+        dynamicArray.removeFirst();
+
+        System.out.println(dynamicArray.get(2));
+        System.out.println(dynamicArray.getLast());
+
+        dynamicArray.remove(2);
+        dynamicArray.add(60);
+
+        System.out.println(dynamicArray.get(2));
     }
 }
