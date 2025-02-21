3485c9f4d5b82907caa9376ff6779057cf330350
abelureste <aureste03@gmail.com>
2025-02-20 20:34:23 -0500

    Added twoSum

---
+++ Java/JavaPractice/src/twoSum.java
@@ -0,0 +1,21 @@
+import java.util.Arrays;        //Arrays method is only needed to use .toString for our println
+
+public class twoSum {
+    public static void main (String[] args) {
+        int[] array = {3, 4, 6, 7};
+        int target = 10;
+
+        System.out.println(Arrays.toString(TwoSum(array, target)));         //Arrays method only needed for this
+    }
+
+    public static int[] TwoSum(int[] nums, int target){
+        for(int i = 0; i < nums.length; i++){
+            for(int j = i + 1; j < nums.length; j++){
+                if(nums[i] + nums[j] == target){
+                    return new int[] {i,j};
+                }
+            }
+        }
+        return new int[0];
+    }
+}
