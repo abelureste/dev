6f1ee5463e770d51af906c31423277f1e858da52
Abel Ureste <aureste03@gmail.com>
2025-02-25 11:50:49 -0500

    Created StackExerciseDemo

---
+++ 3200 Data Structures/JavaDemo/src/CSCI3200/StackExerciseDemo.java
@@ -0,0 +1,41 @@
+package CSCI3200;
+
+import java.util.Scanner;
+import java.util.Stack;
+
+public class StackExerciseDemo {
+    public static void main(String[] args) {
+        Stack<Integer> stack = new Stack<Integer>();
+
+        Scanner userInput = new Scanner(System.in);
+        int count = 1;
+        while(count < 11){
+            System.out.print("Enter an Integer (" + count + ") to add to the stack: ");
+            int stackAdd = userInput.nextInt();
+            stack.push(stackAdd);
+            count++;
+        }
+        userInput.close();
+
+        StackSort(stack);
+        
+    }
+
+    public static void StackSort(Stack<Integer> stack){
+        Stack<Integer> sortedStack = new Stack<Integer>();
+
+        if(stack.isEmpty()){
+            return;
+        }
+
+        for(int i = 0; i < stack.capacity(); i++){
+            int temp = stack.pop();
+            int nextTemp = stack.pop();
+            if(temp < nextTemp){
+                sortedStack.push(temp);
+            }
+            stack = sortedStack;
+        }
+    }
+}
+
