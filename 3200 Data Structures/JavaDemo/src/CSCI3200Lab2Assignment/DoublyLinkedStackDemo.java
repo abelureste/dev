23f0902fa1d34f5b76df43f30824cf45046a8ad2
Abel Ureste <aureste03@gmail.com>
2025-03-09 18:01:51 -0400

    Updated DoublyLinkedStackDemo

--- 3200 Data Structures/JavaDemo/src/CSCI3200Lab2Assignment/DoublyLinkedStackDemo.java
+++ 3200 Data Structures/JavaDemo/src/CSCI3200Lab2Assignment/DoublyLinkedStackDemo.java
@@ -10,17 +10,30 @@ public class DoublyLinkedStackDemo {
         job1.setNextLink(job2);
         job2.setNextLink(job3);
         job3.setNextLink(job4);
+        
         job4.setPrevLink(job3);
         job3.setPrevLink(job2);
         job2.setPrevLink(job1);
 
-        PrintNode<String> current = job1;
-        while(current != job4){
-            System.out.print(current.getJob());
-            if(current.getNextLink() != job4){
-                System.out.print(" --> ");
-            }
-            current = current.getNextLink();
+        //Current webpage
+        PrintNode<String> firstNode = job1;
+        System.out.println("The current webpage is " + firstNode.getJob());
+        //Second webpage
+        while(firstNode != job2) {
+            firstNode = firstNode.getNextLink();
+            System.out.println("The second webpage visited is " + firstNode.getJob());
+        }
+        //Third webpage
+        while(firstNode != job3) {
+            firstNode = firstNode.getNextLink();
+            System.out.println("The third webpage visited is " + firstNode.getJob());
+        }
+        //Backtracking
+        while(firstNode.getPrevLink() != null) {
+            firstNode = firstNode.getPrevLink();
+            if(firstNode.getPrevLink() == null) {
+                System.out.println("The webpage firstNode is pointing to after backtracking is " + firstNode.getJob());
+                }
         }
     }
 }
