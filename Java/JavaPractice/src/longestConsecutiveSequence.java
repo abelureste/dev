2fafa9bc0153cbcd00b60dc6ea9e9c9d10d294c3
abelureste <aureste03@gmail.com>
2025-03-13 16:05:36 -0400

    Updated longestConsecutiveSequence

--- Java/JavaPractice/src/longestConsecutiveSequence.java
+++ Java/JavaPractice/src/longestConsecutiveSequence.java
@@ -7,6 +7,7 @@ public class longestConsecutiveSequence {
         int[] nums = {2,20,4,10,3,4,5};
 
         System.out.println(longestConsecutiveBRUTEFORCE(nums));
+        System.out.println(longestConsecutiveEFFICIENT(nums));
     }
 
     public static int longestConsecutiveBRUTEFORCE(int[] nums) {
@@ -17,7 +18,7 @@ public class longestConsecutiveSequence {
             numbers.add(i);
         }
 
-        for(int i : numbers){
+        for(int i : numbers) {
             int consecutive = 0, current = i;
             while(numbers.contains(current)){
                 consecutive++;
@@ -29,15 +30,23 @@ public class longestConsecutiveSequence {
     }
 
     public static int longestConsecutiveEFFICIENT(int[] nums) {
+        HashSet<Integer> numbers = new HashSet<>();
+        int result = 0;
 
-        for(int i = 0; i < nums.length; i++) {
-            int currentNum = nums[i];
-            if(i > currentNum){
-                
+        for(int i : nums) {
+            numbers.add(i);
+        }
+
+        for(int i : numbers) {
+            if(!numbers.contains(i - 1)){
+                int length = 1;
+                while(numbers.contains(i + length)){
+                    length++;
+                }
+                result = Math.max(length, result);
             }
         }
 
-        int inte = 0;
-        return inte;
+        return result;
     }
 }
