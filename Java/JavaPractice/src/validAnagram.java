cc9e8e1e7eb4aa1d9cd04986f18ad9a2cf0e08a2
abelureste <aureste03@gmail.com>
2025-03-12 22:22:59 -0400

    Updated validAnagram

--- Java/JavaPractice/src/validAnagram.java
+++ Java/JavaPractice/src/validAnagram.java
@@ -1,30 +1,45 @@
 import java.util.Arrays;
+import java.util.HashMap;
 
 public class validAnagram {
     public static void main (String[] args){
         String s = "racecar";
         String t = "carrace";
 
-        System.out.print(isAnagramBRUTEFORCE(s, t));
+        System.out.println(isAnagramBRUTEFORCE(s, t));
+        System.out.println(isAnagramEFFICIENT(s, t));
     }
 
     public static boolean isAnagramBRUTEFORCE(String s, String t){
+        if(s.length() != t.length()) {
+            return false;
+        }
 
         char[] sArray = s.toCharArray();
         Arrays.sort(sArray);
-        String sSorted = new String(sArray);
 
         char[] tArray = t.toCharArray();
         Arrays.sort(tArray);
-        String tSorted = new String(tArray);
 
-        System.out.println(sArray);
-        System.out.println(tArray);
-
-        if(sSorted.equals(tSorted)) {
+        if(Arrays.equals(sArray, tArray)) {
             return true;
         }
 
         else return false;
+    }
+
+    public static boolean isAnagramEFFICIENT(String s, String t){
+        if(s.length() != t.length()) {
+            return false;
+        }
+ 
+        HashMap<Character, Integer> sortedS = new HashMap<>();
+        HashMap<Character, Integer> sortedT = new HashMap<>();
+        for(int i = 0; i < s.length(); i++) {
+            sortedS.put(s.charAt(i), sortedS.getOrDefault(s.charAt(i), 0) + 1);
+            sortedT.put(t.charAt(i), sortedT.getOrDefault(t.charAt(i), 0) + 1);
+        }
+        return sortedS.equals(sortedT);
+
     }
 }
