1fad7db971744fa18323ddf0f0c0e687232842bd
abelureste <aureste03@gmail.com>
2025-03-15 17:01:37 -0400

    Updated validParentheses

--- Java/JavaPractice/src/validParentheses.java
+++ Java/JavaPractice/src/validParentheses.java
@@ -1,13 +1,16 @@
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Stack;
 
 public class validParentheses {
     public static void main(String[] args) {
         String s = "([{}])";
 
-        System.out.println(isValid(s));
+        System.out.println(isValidBRUTEFORCE(s));
+        System.out.println(isValidEFFICIENT(s));
     }
 
-    public static boolean isValid(String s) {
+    public static boolean isValidBRUTEFORCE(String s) {
         while(s.contains("()") || s.contains("{}") || s.contains("[]")) {
             s = s.replace("()", "");
             s = s.replace("{}", "");
@@ -19,5 +22,27 @@ public class validParentheses {
         }
         
         else return false;
+    }
+
+    public static boolean isValidEFFICIENT(String s) {
+        Stack<Character> stack = new Stack<>();
+        Map<Character, Character> closeToOpen = new HashMap<>();
+        closeToOpen.put(')', '(');
+        closeToOpen.put(']' , '[');
+        closeToOpen.put('}', '{');
+
+        for(char i : s.toCharArray()) {
+            if(closeToOpen.containsKey(i)) {
+                if(!stack.isEmpty() && stack.peek() == closeToOpen.get(i)) {
+                    stack.pop();
+                } else {
+                    return false;
+                }
+            } else {
+                stack.push(i);
+            }
+        }
+
+        return stack.isEmpty();
     }
 }
