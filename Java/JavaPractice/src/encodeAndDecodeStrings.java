60149445ecdb9da37747300e862f703bd22f26e5
abelureste <aureste03@gmail.com>
2025-03-24 23:58:56 -0400

    created encodeAndDecodeStrings

---
+++ Java/JavaPractice/src/encodeAndDecodeStrings.java
@@ -0,0 +1,20 @@
+import java.util.List;
+import java.util.Arrays;
+
+public class encodeAndDecodeStrings {
+    public static void main(String[] args) {
+        List<String> stringList = new List<String>();
+        stringList.add("neet");
+        stringList.add("code");
+        stringList.add("love");
+        stringList.add("you");
+    }
+
+    public String encode(List<String> strs) {
+
+    }
+
+    public List<String> decode(String str) {
+
+    }
+}
