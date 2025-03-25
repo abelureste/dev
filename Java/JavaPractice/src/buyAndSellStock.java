224a4a1023b90de9ca3475366373a8b8108e6cd3
Abel Ureste <aureste03@gmail.com>
2025-03-25 13:08:17 -0400

    Updated buyAndSellStock

--- Java/JavaPractice/src/buyAndSellStock.java
+++ Java/JavaPractice/src/buyAndSellStock.java
@@ -3,6 +3,7 @@ public class buyAndSellStock {
         int[] array = {10,1,5,6,7,1};
 
         System.out.println(maxProfit(array));
+        System.out.println(maxProfit2Pointers(array));
         
     }
 
@@ -13,6 +14,23 @@ public class buyAndSellStock {
         for(int i : prices){
             maxPrice = Math.max(maxPrice, i - minBuy);
             minBuy = Math.min(minBuy, i);
+        }
+
+        return maxPrice;
+    }
+
+    public static int maxProfit2Pointers(int[] prices){
+        int left = 0, right = 0;
+        int maxPrice = 0;
+
+        while(right < prices.length) {
+            if(prices[left] < prices[right]) {
+                int profit = prices[right] - prices[left];
+                maxPrice = Math.max(maxPrice, profit);
+            } else {
+                left = right;
+            }
+            right += 1;
         }
 
         return maxPrice;
