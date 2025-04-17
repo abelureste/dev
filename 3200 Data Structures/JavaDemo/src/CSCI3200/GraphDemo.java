a4f09d83e6a03eb82ff958c57b65a3219085710f
Abel Ureste <aureste03@gmail.com>
2025-04-17 11:27:26 -0400

    Created GraphDemo

---
+++ 3200 Data Structures/JavaDemo/src/CSCI3200/GraphDemo.java
@@ -0,0 +1,24 @@
+package CSCI3200;
+
+public class GraphDemo {
+    
+
+    public static void degree (int[][] graph){
+        //display the degree of each vertex. Example, vertex A = 3
+    }
+
+    public static void edgesList (int[][] graph){
+        //display the edges list for this graph using the
+        //adjacency matrix values. Ex, A-B, A-C, A-F
+    }
+
+    public static void adjacencyList (int[][] graph){
+        //display the adjacency list for each vertex in the graph
+        //using the adjacency matrix values. Ex, A --> B(7)-->C(9)-->F(1)
+        //where the values in the parentheses is the weight of the edges.
+    }
+
+    public static void main(String[] args){
+
+    }
+}
