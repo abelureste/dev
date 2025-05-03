b6c9dc71a2dcdaaa700acc2e8a7ef0c17d3f6e54
abelureste <aureste03@gmail.com>
2025-05-03 18:52:59 -0400

    Finished CSCI3200Classlist

--- 3200 Data Structures/JavaDemo/src/CSCI3200Classlist/CSCI3200Classlist.java
+++ 3200 Data Structures/JavaDemo/src/CSCI3200Classlist/CSCI3200Classlist.java
@@ -22,18 +22,97 @@ public class CSCI3200Classlist {
                     count++;
                 }
             }
-            }
-        catch(FileNotFoundException e) {
+        } catch(FileNotFoundException e) {
             System.out.println("File has not been found.");
         }
     }
 
     public static void sortByLastNames(String[] firstNames, String[] lastNames){  
-        
+        String[] firstNamesTemp = new String[37];
+        String[] lastNamesTemp = new String[37];
+
+        for(int i = 0; i < lastNames.length; i++){
+            firstNamesTemp[i] = firstNames[i];
+            lastNamesTemp[i] = lastNames[i];
+        }
+
+        for(int i = 0; i < lastNamesTemp.length; i++){
+            int minimumIndex = i;
+
+            for(int j = i + 1; j < lastNamesTemp.length; j++){
+                if(lastNamesTemp[j].compareToIgnoreCase(lastNamesTemp[minimumIndex]) < 0){
+                    minimumIndex = j;
+                }
+            }
+
+            String tempLast = lastNamesTemp[i];
+            lastNamesTemp[i] = lastNamesTemp[minimumIndex];
+            lastNamesTemp[minimumIndex] = tempLast;
+
+            String tempFirst = firstNamesTemp[i];
+            firstNamesTemp[i] = firstNamesTemp[minimumIndex];
+            firstNamesTemp[minimumIndex] = tempFirst;
+
+        }
+
+        try{
+            File sortedLastNamesFile = new File("Sorted_Last_Names_Classlist.txt");
+            PrintWriter write = new PrintWriter(sortedLastNamesFile);
+
+            for(int i = 0; i < lastNamesTemp.length; i++){
+                write.print(firstNamesTemp[i] + " ");
+                write.println(lastNamesTemp[i]);
+            }
+
+            write.close();
+
+        } catch (IOException e) {
+            System.out.println("An error has occured!");
+        }
     }
 
     public static void sortByFirstNames(String[] firstNames, String[] lastNames){  
-    //add code here  
+        String[] firstNamesTemp = new String[37];
+        String[] lastNamesTemp = new String[37];
+
+        for(int i = 0; i < firstNamesTemp.length; i++){
+            firstNamesTemp[i] = firstNames[i];
+            lastNamesTemp[i] = lastNames[i];
+        }
+
+        for(int i = 0; i < firstNamesTemp.length; i++){
+            int minimumIndex = i;
+
+            for(int j = i + 1; j < firstNamesTemp.length; j++){
+                if(firstNamesTemp[j].compareToIgnoreCase(firstNamesTemp[minimumIndex]) < 0){
+                    minimumIndex = j;
+                }
+            }
+
+            String tempFirst = firstNamesTemp[i];
+            firstNamesTemp[i] = firstNamesTemp[minimumIndex];
+            firstNamesTemp[minimumIndex] = tempFirst;
+
+            String tempLast = lastNamesTemp[i];
+            lastNamesTemp[i] = lastNamesTemp[minimumIndex];
+            lastNamesTemp[minimumIndex] = tempLast;
+
+        }
+
+        try{
+            File sortedFirstNamesFile = new File("Sorted_First_Names_Classlist.txt");
+            PrintWriter write = new PrintWriter(sortedFirstNamesFile);
+
+            for(int i = 0; i < firstNamesTemp.length; i++){
+                write.print(firstNamesTemp[i] + " ");
+                write.println(lastNamesTemp[i]);
+            }
+
+            write.close();
+
+        } catch (IOException e) {
+            System.out.println("An error has occured!");
+        }
     }
 
     public static void main (String[] args){
@@ -42,10 +121,8 @@ public class CSCI3200Classlist {
         String[] lastNames = new String[37];
 
         readValues(firstNames, lastNames);
+        sortByLastNames(firstNames, lastNames);
+        sortByFirstNames(firstNames, lastNames);
 
-        for(int i = 0; i < firstNames.length; i++){
-            System.out.print(firstNames[i] + " ");
-            System.out.println(lastNames[i]);
-        }
     }
 }
